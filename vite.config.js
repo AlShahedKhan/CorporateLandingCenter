94950626aa253552015e9bc8a49fc51736f3fd44
AlShahedKhan <154064318+AlShahedKhan@users.noreply.github.com>
2024-12-09 15:38:12 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  build: {
-    outDir: 'dist', // You can change this to 'dist' if you prefer
-    emptyOutDir: true, // Ensures the build output directory is cleared before building
-  },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
+
